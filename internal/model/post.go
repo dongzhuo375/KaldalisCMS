61c5ddc69efc5d4a656f2d9a08ed632d1222980b
x1carbon <zhouruiqiz@gmail.com>
2025-11-28 20:05:32 +0800

    model update

--- internal/model/post.go
+++ internal/model/post.go
@@ -1,8 +1,17 @@
 package model
 
+import "gorm.io/gorm"
+
 // Post represents the data structure for a blog post.
 type Post struct {
-	ID      int    `json:"id"`
-	Title   string `json:"title"`
-	Content string `json:"content"`
+	gorm.Model
+	Title      string `gorm:"not null"`
+	Slug       string `gorm:"unique;not null"`
+	Content    string
+	Cover      string
+	AuthorID   uint
+	Author     User `gorm:"foreignKey:AuthorID"`
+	CategoryID uint
+	Category   Category `gorm:"foreignKey:CategoryID"`
+	Tags       []Tag    `gorm:"many2many:post_tags;"`
 }
---
+++ internal/model/tag.go
@@ -0,0 +1,10 @@
+package model
+
+import "gorm.io/gorm"
+
+type Tag struct {
+	gorm.Model
+	Name  string `gorm:"unique;not null"`
+	Slug  string `gorm:"unique;not null"`
+	Posts []Post `gorm:"many2many:post_tags;"`
+}
